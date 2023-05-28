10bfbee3672f7c6f742226a6f0a9ae68c19d4c18
wymansen67 <DenisGorbatuk77@gmail.com>
2023-05-28 17:08:01 +0500

    2nd commit

---
+++ Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Department
+    {
+        public string name;
+        public uint baseSalary;
+        public byte coefficient;
+
+        public string Name { get; set; }
+        public uint BaseSalary { get; set; }
+        public byte Coefficient { get; set; }
+
+        public Department() { }
+    }
+}
